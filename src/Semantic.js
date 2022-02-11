1e8ce044f145d4bf6a1c46409fe84a30213f2036
pujitha <siripujitha9701@gamil.com>
2022-02-11 22:35:57 +0530

    REACT-JS

---
+++ src/Semantic.js
@@ -0,0 +1,13 @@
+function Semantic(){
+    return(
+        <div className="one">
+              <h1> Welcome to Morioh!</h1>
+              <h3>Let's write,share knowledge and earn <a>GEEK</a></h3>
+              <button class="ui primary button">
+             Sign UP
+              </button>
+        </div>
+    )
+}
+
+export default Semantic;
